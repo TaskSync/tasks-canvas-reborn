55c3d7f8fa5794de09b907b3d57eb342ff8e4b13
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2019-12-21 21:53:17 +0100

    - keyboard navigation - text editing

--- dev/protos/listui.tsx
+++ dev/protos/listui.tsx
@@ -1,19 +1,12 @@
 import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import React, { KeyboardEvent, MouseEvent, useState, useReducer } from "react";
 import ReactDOM from "react-dom";
-import {
-  createStyles,
-  makeStyles,
-  createMuiTheme,
-  Theme
-} from "@material-ui/core/styles";
-import CommentIcon from "@material-ui/icons/Comment";
-import React from "react";
+import classnames from "classnames";
 
 // const theme = createMuiTheme();
 
@@ -24,60 +17,115 @@ const useStyles = makeStyles((theme: Theme) =>
       maxWidth: 360,
       backgroundColor: "white"
     },
-    ListItem: {
-      background: "red"
+    row: {
+      height: "1.5em"
+    },
+    selected: {
+      background: "grey"
     }
   })
 );
 
-export default function ListUI() {
-  const classes = useStyles({});
-  const [checked, setChecked] = React.useState([0]);
+export type TTask = {
+  id: string;
+  text: string;
+};
 
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+const tasks: TTask[] = [
+  { id: "id-0", text: "test 1" },
+  { id: "id-1", text: "test 2" },
+  { id: "id-2", text: "test 3" },
+  { id: "id-3", text: "test 4" }
+];
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  {
-    /*<ThemeProvider>*/
+type Action = { type: "update"; task: TTask };
+function tasksReducer(state: TTask[], action: Action) {
+  switch (action.type) {
+    case "update":
+      const task = state.find(task => task.id === action.task.id);
+      task.text = action.task.text;
+      return [...state];
   }
+}
+
+export default function TaskList({ tasks }: { tasks: TTask[] }) {
+  const classes = useStyles({});
+  const [checked, setChecked] = useState([]);
+  const [focusedID, setFocusedID] = useState(null);
+  const [list, dispatchList] = useReducer(tasksReducer, tasks);
+
+  function getTaskByID(id: string): TTask {
+    return list.find(task => task.id === id);
+  }
+
+  function handleKey(id: string) {
+    return (event: KeyboardEvent<HTMLElement>) => {
+      const task = getTaskByID(id);
+      if (["ArrowDown", "ArrowUp"].includes(event.key)) {
+        const index = list.indexOf(task);
+        console.log("task", task.text);
+        let indexChanged;
+        // navigate between tasks
+        if (event.key === "ArrowDown") {
+          // move down
+          indexChanged = Math.min(index + 1, list.length - 1);
+        } else {
+          // move up
+          indexChanged = Math.max(index - 1, 0);
+        }
+        setFocusedID(list[indexChanged].id);
+        event.preventDefault();
+      } else {
+        // edit text
+        if (event.key === "Tab") {
+          // catch tab for text input
+          event.preventDefault();
+        }
+        task.text += String.fromCharCode(event.keyCode);
+        dispatchList({ type: "update", task });
+      }
+    };
+  }
+
+  function handleClick(id: string) {
+    return (event: MouseEvent<HTMLElement>) => {
+      setFocusedID(id);
+      event.preventDefault();
+    };
+  }
+
   return (
     <List className={classes.root}>
-      {[0, 1, 2, 3].map(value => {
-        const labelId = `checkbox-list-label-${value}`;
+      {list.map(task => {
+        const { id, text } = task;
+        const labelId = `checkbox-list-label-${id}`;
+        const isSelected = id === focusedID;
 
         return (
           <ListItem
-            key={value}
+            className={classnames(
+              classes.row,
+              isSelected ? classes.selected : null
+            )}
+            key={id}
             role={undefined}
             dense
             button
-            onClick={handleToggle(value)}
+            onClick={handleClick(id)}
+            onKeyDown={handleKey(id)}
+            selected={id === focusedID}
+            autoFocus={focusedID === id}
           >
             <ListItemIcon>
               <Checkbox
                 edge="start"
-                checked={checked.indexOf(value) !== -1}
+                checked={checked.includes(id)}
                 tabIndex={-1}
                 disableRipple
                 inputProps={{ "aria-labelledby": labelId }}
               />
             </ListItemIcon>
-            <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
+            <ListItemText id={labelId} primary={text} />
           </ListItem>
         );
       })}
@@ -86,5 +134,5 @@ export default function ListUI() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<ListUI />, document.body as HTMLElement);
+  ReactDOM.render(<TaskList tasks={tasks} />, document.body as HTMLElement);
 });
