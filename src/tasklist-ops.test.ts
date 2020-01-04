1cdbd09c6c8a943f3094b94d87c90b8203dd8aab
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2020-01-04 17:10:30 +0100

    - tests for user sorting

--- src/tasklist-ops.test.ts
+++ src/tasklist-ops.test.ts
@@ -1,47 +1,43 @@
 import { TTask } from "./store";
 import { sortTasks, now, isUserSorted } from "./tasklist-ops";
 // @ts-ignore
-import deepcopy from "deepcopy";
 
 describe("sorting", () => {
   describe("user", () => {
     const sorted: TTask[] = [
-      {
-        id: "id-0",
-        title: "abc",
-        updated: now(),
-        created: now()
-      },
-      {
-        id: "id-1",
-        parent: "id-0",
-        title: "123 456",
-        updated: now(),
-        created: now()
-      },
-      {
-        id: "id-2",
-        previous: "id-0",
-        title: "test 3",
-        updated: now(),
-        created: now()
-      },
-      {
-        id: "id-3",
-        previous: "id-2",
-        title: "test 4",
-        updated: now(),
-        created: now()
-      }
+      createTask("1"),
+      createTask("2", { parent: "1" }),
+      createTask("3", { previous: "1" }),
+      createTask("4", { previous: "3" })
+    ];
+
+    const unsorted: TTask[] = [
+      createTask("2", { parent: "1" }),
+      createTask("1"),
+      createTask("3", { previous: "1" }),
+      createTask("4", { previous: "3" })
     ];
 
     test("isUserSorted", () => {
       expect(isUserSorted(sorted)).toBeTruthy();
-      const unsorted = deepcopy(sorted);
-      let tmp = unsorted[0];
-      unsorted[0] = unsorted[1];
-      unsorted[1] = tmp;
-      expect(isUserSorted(sorted)).toBeFalsy();
+      expect(isUserSorted(unsorted)).toBeFalsy();
+    });
+
+    test("sortTasks", () => {
+      expect(sortTasks(sorted)).toMatchObject(sorted);
+      expect(sortTasks(unsorted)).toMatchObject(sorted);
     });
   });
 });
+
+// text is also an ID, has to be unique
+function createTask(text: string, task?: Partial<TTask>): TTask {
+  return {
+    id: text,
+    title: text,
+    updated: now(),
+    created: now(),
+    previous: task?.previous || undefined,
+    parent: task?.parent || undefined
+  };
+}
